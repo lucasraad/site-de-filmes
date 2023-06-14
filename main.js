594b94658285050cc898e04bc2beb65a9d01623e
lucasraad <119633018+lucasraad@users.noreply.github.com>
2023-06-13 23:37:33 -0300

    alterações finais

---
+++ main.js
@@ -0,0 +1,43 @@
+function buscar() {
+    const apikey = '8fa8be48' ; 
+    const frmPesquisa = document.querySelector("form");
+
+frmPesquisa.onsubmit = (ev) => {
+    ev.preventDefault();
+
+    const pesquisa = ev.target.pesquisa.value;
+
+    if (pesquisa == "") {
+    alert ('preencha o caminho!');
+    return;
+
+
+    }
+    fetch(`https://www.omdbapi.com/?s=${pesquisa}&apikey=${apikey}`)
+    .then(result => result.json())
+    .then(json => carregaLista(json));
+
+}
+const carregaLista = (json) => {
+    const lista = document.querySelector("div.lista");
+    lista.innerHTML = "";
+
+    if (!json.Response) {
+        alert('nenhuma filme encontrado');
+    }
+
+    json.Search.forEach(Element => {
+        console.log(Element);
+
+        let item = document.createElement("div");
+        item.classList.add("item");
+
+        item.innerHTML = `<img src="${Element.Poster}" /><h2>${Element.Title}</h2>`
+        
+        lista.appendChild(item);
+    })
+
+
+
+}
+}
